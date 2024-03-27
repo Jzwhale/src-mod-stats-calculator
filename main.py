00a9b71e3be3bfee1af04c7a1b861e7b6111f571
JONALD <jzwhale@gmail.com>
2024-03-27 01:40:08 -0500

    Added comments

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-# VARIABLES ############################################################################################################
+# VARIABLES #################################################################################
 
 examiner_name = "Awesome_Username"
 
-########################################################################################################################
+#############################################################################################
 
 import requests
 from time import sleep
@@ -15,42 +15,49 @@ except:
     print("ERROR: There is no user with this name!")
     exit()
 
+# Starting link, for all verified runs examined by specified user, offset by 0
 link = f"https://www.speedrun.com/api/v1/runs?examiner={examiner_id}&status=verified&orderby=verify-date&offset=0"
 
 total_time = 0
 run_count = 0
-
 longest_run = 0
 
 while True:
 
     result = requests.get(link)
     
+    # This just assumes you've been rate limited. Might be another problem though (hopefully not..)
     if result.status_code != 200:
         print(f"RATE LIMITED! WAITING 30 SECONDS.")
         sleep(30)
         continue
     
+    # Looping over the runs on this page
     info = result.json()
     for entry in info["data"]:
     
+        # This block pretty much just skips over "N/A" runs
         run_should_be_considered = True
         for player in entry["players"]:
             if player["rel"] == "guest" and player["name"].upper() == "N/A":
                 run_should_be_considered = False
                 break
-
         if not run_should_be_considered: continue
 
+        # Adds to the total time
         time = entry["times"]["realtime_t"]
         total_time += time
+
+        # Assigns new longest run time
         if time > longest_run: 
             longest_run = time
             print(f"New Longest Run Found: {entry["weblink"]}")
+        
         run_count += 1
 
     print("Calculating total time... {0:.2f} (Run #{1})".format(total_time, run_count))
 
+    # Changes link to get next page, or stops if complete
     if info["pagination"]["size"] < 20:
         break
     else:
@@ -61,10 +68,12 @@ while True:
 
     sleep(0.3)
 
+    # Yields for 15 seconds every 1000 runs checked to avoid rate limits
     if int(re.search(r"\d+$", link).group()) % 1000 == 0 and run_count != 0:
         print("Resuming in 15 seconds... (avoiding rate limits)")
         sleep(15)
 
+# STATS!!!!
 hours = total_time / 3600
 
 average_length = total_time / run_count
